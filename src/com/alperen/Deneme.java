90e0ee6901fa1b493aa7737c51d2091a5adbd20f
alper <alper@DESKTOP-5PGMSII>
2023-05-16 14:53:19 +0300

    first commit

---
+++ src/com/alperen/Deneme.java
@@ -0,0 +1,11 @@
+package com.alperen;
+
+public class Deneme {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author alper
+ *
+ */
+module GitDeneme {
+}
